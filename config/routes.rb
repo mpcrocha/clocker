8a5dea1100e0d6d762b271d0e0abca1f9982a7a3
marcos_rocha <marcos.rocha@m4u.com.br>
2019-12-25 12:11:29 -0500

    [WIP] Adding devise for login

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
+  get 'home/home'
+  get 'home/index'
+  devise_for :users
+
+  devise_scope :user do
+    authenticated :user do
+      root 'home#index', as: :authenticated_root
+    end
+
+    unauthenticated do
+      root 'devise/sessions#new', as: :unauthenticated_root
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
